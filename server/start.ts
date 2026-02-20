15fd62861b3aa5e8cf8c4d14483e8501896ca1f5
PoorvashaV <131988994+PoorvashaV@users.noreply.github.com>
2026-02-20 17:35:43 +0530

    Update start.ts

--- server/start.ts
+++ server/start.ts
@@ -1,8 +1,10 @@
-import { createServer } from "./index.js";
+// FIX: Remove .js extension
+import { createServer } from "./index";
 
 const app = createServer();
+// Render uses the PORT environment variable
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Backend running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Backend running on port ${PORT}`);
 });
